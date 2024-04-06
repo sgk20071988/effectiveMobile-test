ea7726798f538aca2ba166c377703fdcc1a748c7
sgk1988 <georg@science.ssti.ru>
2024-04-06 16:22:04 +0700

    Add get car by regNum route

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,10 +4,34 @@ import (
 	"effectiveMobileTest/internal/db"
 	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func GetCars(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(db.Get())
+}
+
+func GetCar(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	for _, item := range db.Get() {
+		if item.RegNum == params["regNum"] {
+			w.WriteHeader(http.StatusOK)
+			// add a arbitraty pause of 1 second
+			time.Sleep(1000 * time.Millisecond)
+			if err := json.NewEncoder(w).Encode(item); err != nil {
+				panic(err)
+			}
+			return
+		}
+	}
+	// If we didn't find it, 404
+	w.WriteHeader(http.StatusNotFound)
+	if err := json.NewEncoder(w).Encode(jsonError{Message: "Not Found"}); err != nil {
+		panic(err)
+	}
 }
