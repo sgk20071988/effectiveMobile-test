7dfe37daae1c82f12428a1d866c8db00801af5eb
sgk1988 <georg@science.ssti.ru>
2024-04-06 16:06:00 +0700

    add db rep. Add GetCars

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,1 +1,13 @@
 package api
+
+import (
+	"effectiveMobileTest/internal/db"
+	"encoding/json"
+	"net/http"
+)
+
+func GetCars(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(db.Get())
+}
