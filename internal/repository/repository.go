ed345ea16fe0b965b22af68f1904c8549d50bff2
sgk1988 <georg@science.ssti.ru>
2024-04-07 22:23:43 +0700

    Remade repository to go-sqlbuilder

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"database/sql"
 	model "effectiveMobileTest/internal/model"
+
+	"github.com/huandu/go-sqlbuilder"
 )
 
 type Repository struct {
@@ -10,26 +12,36 @@ type Repository struct {
 }
 
 func (r *Repository) Insert(car model.Car) error {
-	query := "insert into Cars (regNum,mark,model,owner) values ($1,$2,$3,$4)"
+	queryBuilder := sqlbuilder.PostgreSQL.NewInsertBuilder()
+	queryBuilder.InsertInto("Car")
+	queryBuilder.Cols("mark", "model", "owner", "regNum")
+	queryBuilder.Values(car.Mark, car.Model, car.Owner, car.RegNum)
+	sql, args := queryBuilder.Build()
 	if _, err := r.DB.Exec(
-		query,
-		car.RegNum,
-		car.Mark,
-		car.Model,
-		car.Owner,
+		sql,
+		args,
 	); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (r *Repository) GetCars(limit, offset int) (cars []model.Car, e error) {
-	query := "select regNum,mark,model,owner from Cars LIMIT $1 OFFSET $1"
+func (r *Repository) GetCars(limit, offset int, conds map[string]string) (cars []model.Car, e error) {
+	sb := sqlbuilder.PostgreSQL.NewSelectBuilder()
+	sb.From("Cars")
+	sb.Select("regNum", "mark", "model", "owner")
+	sb.Limit(limit)
+	sb.Offset(offset)
+	for col, like := range conds {
+		sb.Where(
+			sb.Like(col, like),
+		)
+	}
+	query, args := sb.Build()
 
 	rows, err := r.DB.Query(
 		query,
-		limit,
-		offset,
+		args,
 	)
 
 	if err != nil {
@@ -54,17 +66,39 @@ func (r *Repository) GetCars(limit, offset int) (cars []model.Car, e error) {
 }
 
 func (r *Repository) DeleteCar(regNum string) error {
-	query := "delete from Cars where regNum = $1"
+	db := sqlbuilder.PostgreSQL.NewDeleteBuilder()
+	db.DeleteFrom("Cars")
+	db.Where(
+		db.EQ("regNum", regNum),
+	)
+	query, args := db.Build()
 	if _, err := r.DB.Exec(
 		query,
-		regNum,
+		args,
 	); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (r *Repository) Update(regNum string, args map[string]string)
-{
-	
+func (r *Repository) Update(regNum string, update map[string]string) error {
+	ub := sqlbuilder.PostgreSQL.NewUpdateBuilder()
+	ub.Update("Cars")
+	for col, val := range update {
+		ub.Set(
+			ub.Assign(col, val),
+		)
+	}
+	ub.Where(
+		ub.Equal("regNum", regNum),
+	)
+
+	query, args := ub.Build()
+	if _, err := r.DB.Exec(
+		query,
+		args,
+	); err != nil {
+		return err
+	}
+	return nil
 }
