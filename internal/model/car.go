2e616def43e34f15fbac3417c0294e5329dd2313
sgk1988 <georg@science.ssti.ru>
2024-04-09 22:39:34 +0700

    Remove ignored files

--- internal/model/car.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Car struct {
-	RegNum string `json: regNum`
-	Mark   string `json: mark`
-	Model  string `json: model`
-	Owner  Person `json: owner`
-	Year   int    `json: year`
-}
--- internal/model/person.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Person struct {
-	Name       string `json: name`
-	SurName    string `json: surname`
-	Patronymic string `json: patronymic`
-}
